abfcdbff5fc3812822204575203e99605b04a700
JCodeTalker <jonesoaresdasilva@gmail.com>
2022-04-09 21:00:42 -0300

    Update styles.js

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,13 +1,6 @@
 import styled from 'styled-components/native'
 
 export const Container = styled.View`
-/* display: flex; */
 flex-direction: row;
-/* align-items: center; */
 justify-content: center;
-/* padding: 10% 40%; */
-/* width: 100%; */
-/* background-color: dark-gray; */
-/* padding: 10px 220px; */
-/* margin: 0 10%; */
 `
